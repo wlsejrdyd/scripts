4204e24ba54e99d81f2ac8870822c613c45d92ef
찐떡 <130738855+wlsejrdyd@users.noreply.github.com>
2023-09-14 15:06:23 +0900

    Update newvmstart_v1.2.sh
    9 버전이 나오면서 변경 된 permitrootlogin 옵션 수정이 필요하여 수정함.

--- newvmstart_v1.1.sh
+++ newvmstart_v1.1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ### Variables
-ranpw=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9!#%^*()_+'|fold -w 11 |sed 1q)
+ranpw=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9!#%^*()_+'|fold -w 10 |sed 1q)
 
 ### Banner
 bann ()
@@ -19,20 +19,65 @@ if [ $(cat /etc/passwd | grep ${user1}|wc -l) -eq 0 ] ; then
 			echo "[FAILED] YOU MUST ENTER COMMENTS. TRY AGAIN."
 			exit 1
 		fi
+generate_password() {
+    local length="$1"
+    local has_upper=false
+    local has_lower=false
+    local has_digit=false
+    local has_special=false
+
+    while true; do
+        password=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9!#%^*' | fold -w "$length" | sed 1q)
+
+        for ((i = 0; i < length; i++)); do
+            char="${password:$i:1}"
+            if [[ "$char" =~ [A-Z] ]]; then
+                has_upper=true
+            elif [[ "$char" =~ [a-z] ]]; then
+                has_lower=true
+            elif [[ "$char" =~ [0-9] ]]; then
+                has_digit=true
+            else
+                has_special=true
+            fi
+        done
+
+        if $has_upper && $has_lower && $has_digit && $has_special; then
+            echo "$password"
+            break
+        fi
+    done
+}
+
+	ranpw=$(generate_password 10)
 	echo -e "[INFO] Use password? (Copy & Paste) : $ranpw"
 	/usr/sbin/useradd ${user1} -c "${comm1}" && passwd ${user1}
 	echo -e "[INFO] passwd files..\n$(/usr/bin/cat /etc/passwd | grep ${user1})"
 	echo -e "[INFO] ${user1}\n$(/usr/bin/chage -l ${user1}|head -2)"
 	echo -e "${user1} ALL=(ALL) ALL" > /etc/sudoers.d/${user1}
-	ssh1=$(/usr/bin/cat /etc/ssh/sshd_config | grep -i "permitrootlogin no"|grep -v "^#" | wc -l)
-	ssh2=$(/usr/bin/cat /etc/ssh/sshd_config |grep -i "permitrootlogin yes"|grep -v "^#" | wc -l)
-		if [ ${ssh1} -eq 0 ] || [ ${ssh2} -eq 1 ]  ; then
-			/usr/bin/sed -i "/^PermitRoot/d" /etc/ssh/sshd_config
-			echo "PermitRootLogin no" >> /etc/ssh/sshd_config
-			systemctl restart sshd
-		else
-			echo ""
-		fi
+	if [ -f "/etc/ssh/sshd_config.d/01-permitrootlogin.conf" ] ; then
+		ssh1=$(/usr/bin/cat /etc/ssh/sshd_config.d/01-permitrootlogin.conf | grep -i "permitrootlogin no"|grep -v "^#" | wc -l)
+		ssh2=$(/usr/bin/cat /etc/ssh/sshd_config.d/01-permitrootlogin.conf |grep -i "permitrootlogin yes"|grep -v "^#" | wc -l)
+			if [ ${ssh1} -eq 0 ] || [ ${ssh2} -eq 1 ]  ; then
+				/usr/bin/sed -i "/^PermitRoot/d" /etc/ssh/sshd_config.d/01-permitrootlogin.conf
+				echo "PermitRootLogin no" >> /etc/ssh/sshd_config.d/01-permitrootlogin.conf
+				systemctl restart sshd
+			else
+				echo ""
+			fi
+	else
+		ssh1=$(/usr/bin/cat /etc/ssh/sshd_config | grep -i "permitrootlogin no"|grep -v "^#" | wc -l)
+		ssh2=$(/usr/bin/cat /etc/ssh/sshd_config |grep -i "permitrootlogin yes"|grep -v "^#" | wc -l)
+			if [ ${ssh1} -eq 0 ] || [ ${ssh2} -eq 1 ]  ; then
+				/usr/bin/sed -i "/^PermitRoot/d" /etc/ssh/sshd_config
+				echo "PermitRootLogin no" >> /etc/ssh/sshd_config
+				systemctl restart sshd
+			else
+				echo ""
+			fi
+	fi	
+else
+	echo "user ${user1} already exists"
 fi
 ;;
 2 | firewall )
