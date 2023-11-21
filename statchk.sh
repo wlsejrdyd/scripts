e945847c186949a1b2de5cfd59831904292ddded
찐떡 <130738855+wlsejrdyd@users.noreply.github.com>
2023-11-21 15:53:44 +0900

    Update statchk.sh
    1

--- statchk.sh
+++ statchk.sh
@@ -7,8 +7,8 @@ dir1="/var/named"
 tm1=`date +"%Y%m%d"`
 #tm2=`date +"%Y%m%d%H%M"`
 tm3=`date +"%Y%m%d" -d '7days ago'`
-var1=`ping -c1 hisapp.snuh.org|head -2|xargs -I{} date '+%F %T{}'  && ping -c1 hisweb.snuh.org|head -2|xargs -I{} date '+%F %T{}' && ping -c1 hisimg.snuh.org |head -2|xargs -I{} date '+%F %T{}' && ping -c1 hisreport.snuh.org |head -2|xargs -I{} date '+%F %T{}'`
-dom1="hisapp.snuh.org hisimg.snuh.org hisweb.snuh.org hisreport.snuh.org"
+var1=`ping -c1 hisapp.deok.kr|head -2|xargs -I{} date '+%F %T{}'  && ping -c1 hisweb.deok.kr|head -2|xargs -I{} date '+%F %T{}' && ping -c1 hisimg.deok.kr |head -2|xargs -I{} date '+%F %T{}' && ping -c1 hisreport.deok.kr |head -2|xargs -I{} date '+%F %T{}'`
+dom1="hisapp.deok.kr hisimg.deok.kr hisweb.deok.kr hisreport.deok.kr"
 
 ### Ping check
 for i in $dom1
