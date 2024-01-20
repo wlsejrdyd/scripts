1e8818a6fd2f17f03c235321eeecdaf00b47cdcb
찐떡 <130738855+wlsejrdyd@users.noreply.github.com>
2024-01-20 18:57:03 +0900

    Update reload.sh

--- reload.sh
+++ reload.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 bann ()
-{ echo -e "## Make by. jindeokyong ##\n[INFO] HIS IP LIST\n1. hisapp : 1.1.1.1\n2. hisimg : 2.2.2.2\n3. hisreport : 3.3.3.3\n4. hisweb : 4.4.4.4\n\n[INFO] HIS DR IP LIST\n1. hisapp : 5.5.5.5\n2. hisimg : 6.6.6.6\n3. hisreport : 7.7.7.7\n4. hisweb : 8.8.8.8
+{ echo -e "## Make by. jindeokyong ##\n[INFO] Sub Domain IP LIST\n1. sub1 : 1.1.1.1\n2. sub2 : 2.2.2.2\n3. sub3 : 3.3.3.3\n4. sub4 : 4.4.4.4\n\n[INFO] HIS DR IP LIST\n1. sub1 : 5.5.5.5\n2. sub2 : 6.6.6.6\n3. sub3 : 7.7.7.7\n4. sub4 : 8.8.8.8
 \n\033[0;31m## [WARNING] Delete existing records and add new records. ##\033[0m"
 }
 bann
@@ -9,40 +9,41 @@ bann
 todo ()
 {
 echo -e "[INFO] zone file backed up here : $dir1/bak/$tm1/${file1}_${tm2}"
-cp -ar $dir1/$file1 $dir1/bak/$tm1/${file1}_${tm2}
+sudo cp -ar $dir1/$file1 $dir1/bak/$tm1/${file1}_${tm2}
 echo -e "[OK] subdns IP change of $var1 , $var2 , $var3 , $var4 completed."
-sed -i '/hisapp\|hisweb\|hisimg\|hisreport/d' $dir1/$file1
+sudo sed -i '/sub1\|sub4\|sub2\|sub3/d' $dir1/$file1
 }
 
 ### Variables
-var1="hisweb"
-var2="hisapp"
-var3="hisimg"
-var4="hisreport"
+var1="sub1"
+var2="sub2"
+var3="sub3"
+var4="sub4"
 dir1="/var/named"
 tm1=`date +"%Y%m%d"`
 tm2=`date +"%Y%m%d%H%M"`
 tm3=`date +"%Y%m%d" -d '90days ago'`
-file1="snuh.org.zone"
+dns1="deok.kr"
+file1="${dns1}.zone"
 if [ ! -d "$dir1/bak/$tm1" ]; then
-	mkdir -p $dir1/bak/$tm1
+	sudo mkdir -p $dir1/bak/$tm1
 fi
 ### Zone File Edit
-echo -e "\033[0;32m[Select]\n\033[0;33m1. HIS -> DR\n2. DR -> HIS\n3. Cancel \033[0m"
+echo -e "\033[0;32m[Select]\n\033[0;33m1. Service system -> DR\n2. DR -> Service system \n3. Cancel \033[0m"
 read read1
 case "$read1" in 1 )
 todo
-echo "hisapp                  IN A            5.5.5.5   ; HIS App DR Service,Jung Hyun Chul, T.4068, DATE 220610
-hisimg                  IN A            6.6.6.6   ; DR HIS Image Web Service, Jung Hyun Chul, T.4068, DATE 220610
-hisreport               IN A            7.7.7.7   ; DR HIS Report Service, Jung Hyun Chul, T.4068, DATE 220610
-hisweb                  IN A            8.8.8.8   ; DR HIS Integration Web Service, Jung Hyun Chul, T.4068, DATE 220610" >> $dir1/$file1
+sudo echo "sub1                  IN A            5.5.5.5   ;
+sub2                  IN A            6.6.6.6   ;
+sub3               IN A            7.7.7.7   ;
+sub4                  IN A            8.8.8.8   ;" >> $dir1/$file1
 ;;
 2 )
 todo
-echo "hisapp                  IN A            1.1.1.1    ; HIS App Service, Moon Gui Sun, T.4339, DATE 160627
-hisimg                  IN A            2.2.2.2     ; HIS Image Web Service, Moon Gui Sun, T.4339, DATE 160627
-hisreport               IN A            3.3.3.3    ; HIS Report Service, Moon Gui Sun, T.4339, DATE 160627
-hisweb                  IN A            4.4.4.4    ; HIS Integration Web Service, Moon Gui Sun, T.4339, DATE 160627" >> $dir1/$file1
+sudo echo "sub1                  IN A            1.1.1.1    ;
+sub2                  IN A            2.2.2.2     ;
+sub3               IN A            3.3.3.3    ;
+sub4                  IN A            4.4.4.4    ;" >> $dir1/$file1
 ;;
 * | 3 )
 exit
@@ -58,10 +59,10 @@ date=$(grep -i serial ${dir1}/${file1} | sed 's/^ *\(.*\) *;*$/\1/' |cut -d ";" 
 #yyyymmdd=$(echo $date | sed 's/\(..\)$//')
 yyyymmdd=$(echo $date | sed 's/\(..\)/\1/')
 newdate1=$(echo $yyyymmdd | sed "s@${yyyymmdd}@$(date +%Y%m%d%M)@g")
-sed -i "s@${yyyymmdd}@${newdate1}@g" ${dir1}/${file1}
+sudo sed -i "s@${yyyymmdd}@${newdate1}@g" ${dir1}/${file1}
 named-checkzone snuh.org $dir1/$file1
 if [ "$?" == "0" ]; then
-	systemctl reload named
+	sudo systemctl reload named
 	echo "[OK] named service reload complete, please wait 10 second."
 	sleep 10
 	sudo sh /home/jindeokyong/statchk.sh pingchk
