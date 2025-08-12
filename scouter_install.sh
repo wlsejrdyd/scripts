86da73907aca42be1f79bba1007165723ca19545
찐떡 <130738855+wlsejrdyd@users.noreply.github.com>
2025-08-12 14:59:05 +0900

    Update scouter_install.sh
    agent java 설정 까지 추가.. (나중에 수정할 것 같음)

--- scouter_install.sh
+++ scouter_install.sh
@@ -2,17 +2,17 @@
 # made by jindeokyong
 
 echo "[NOTICE] 스카우터 설치 스크립트를 실행 합니다. 문의 사항은 별도 연락 부탁드립니다." && sleep 1
-dir1="/app/scouter"
-infotxt="/tmp/info_scouter.txt"
-svcid="scoutersvc"
+sc_dir="/app/scouter"
+sc_infotxt="/tmp/info_scouter.txt"
+sc_svcid="scoutersvc"
 
 if [ $(id -u) -ne "0" ] ; then
-        echo "[WARNING] root 계정으로 실행 해주시길 바랍니다."
-        exit 1
+  echo "[WARNING] root 계정으로 실행 해주시길 바랍니다."
+  exit 1
 fi
 
-read -p "[NOTICE] 설치 디렉토리는 /app/scouter 로 \"고정\" 입니다. (y/n) : " var1
-case $var1 in
+read -p "[NOTICE] 설치 디렉토리는 /app/scouter 로 \"고정\" 입니다. (y/n) : " var
+case $var in
   y)
     echo "동의 하신다면 엔터를 눌러주세요."
     read
@@ -27,9 +27,9 @@ case $var1 in
 esac
 
 ## 계정 생성
-if [ $(cat /etc/passwd | grep $svcid | wc -l) -eq 0 ] ; then
+if [ $(cat /etc/passwd | grep $sc_svcid | wc -l) -eq 0 ] ; then
   echo "[INFO] 서비스 계정을 생성 합니다."
-  useradd -c "Scouter Service Account ID" -s /sbin/nologin $svcid
+  useradd -c "Scouter Service Account ID" -s /sbin/nologin $sc_svcid
 else
   echo "[INFO] 이미 계정이 있습니다."
 fi
@@ -41,13 +41,13 @@ if [ ! -d "/app/scouter" ] ;
     mkdir -p /app
     find1=`find /* -type f -name "jdy-scouter*" -exec echo "압축 파일 경로 : {}" \;`
     echo "$find1"
-    read -p "/app 경로에 압축 해제 하시겠습니까? (y/n) : " var2
-    case $var2 in
+    read -p "/app 경로에 압축 해제 하시겠습니까? (y/n) : " var
+    case $var in
       y )
           echo -n "압축 파일 경로 입력 해주세요. ($find1) : "
-          read path1
-          tar xvfz $path1 -C /app/
-          chown -R $svcid:$svcid $dir1
+          read path
+          tar xvfz $path -C /app/
+          chown -R $sc_svcid:$sc_svcid $sc_dir
           if [ $? -eq 0 ] ;
           then
             echo "[INFO] 압축 해제가 성공 하였습니다."
@@ -59,31 +59,31 @@ if [ ! -d "/app/scouter" ] ;
       n )
     esac
   else
-    echo "[NOTICE] 디렉터리가 이미 존재 합니다, 설치를 진행 합니다."
+    echo "[INFO] 디렉터리가 이미 존재 합니다, 설치를 진행 합니다."
     sleep 2
 fi
 
 ## 스카우터 설치 진행
 while true ; do
   echo -e "[SELECT]\n(1) Configuration Scouter Server\n(2) Configuration Scouter agent.host\n(3) Configuration Scouter agent.java\n(0) exit"
-  read -p "(0~3) : " var3
-    case $var3 in
+  read -p "(0~3) : " var
+    case $var in
       1)
         echo "[INFO] Scouter-Server 설정를 시작 합니다."
-        if [ ! -f "$dir1/server/conf/scouter.conf" ] ; then
+        if [ ! -f "$sc_dir/server/conf/scouter.conf" ] ; then
           echo "[WARNING] 설정파일이 없습니다."
         fi
-        read -p "사용하실 Scouter-Server 이름을 입력 해주세요 (ex : jdy-scouter-server-01) : " serverid1
-        sed -i "s@serverid@$serverid1@g" $dir1/server/conf/scouter.conf
-        find $dir1/server -type f -name "scouter.conf" -exec echo "==== PATH : {} ==== $(date)" \; -exec cat {} \; | grep -v "^#\|^$" >> $infotxt
+        read -p "사용하실 Scouter-Server 이름을 입력 해주세요 (ex : jdy-scouter-server-01) : " serverid
+          sed -i "s@serverid@$serverid@g" $sc_dir/server/conf/scouter.conf && sleep 1
+        find $sc_dir/server -type f -name "scouter.conf" -exec echo "==== PATH : {} ==== $(date)" \; -exec cat {} \; | grep -v "^#\|^$" >> $sc_infotxt
         echo -e "[Unit]
 Description=Scouter Server Service
 After=network.target
 
 [Service]
-WorkingDirectory=/app/scouter/server
-User=$svcid
-Group=$svcid
+WorkingDirectory=$sc_dir/server
+User=$sc_svcid
+Group=$sc_svcid
 ExecStart=/app/jdk/bin/java -Xmx512m -classpath ./scouter-server-boot.jar scouter.boot.Boot ./lib
 ExecStop=/usr/bin/rm -f *.scouter
 Restart=always
@@ -92,11 +92,11 @@ StandardError=journal
 
 [Install]
 WantedBy=multi-user.target" > /usr/lib/systemd/system/scouter-server.service
-        find /usr/lib/systemd/system/ -name "scouter-server.service" -exec echo "==== PATH : {} ==== $(date)" \; -exec cat {} \; >> $infotxt
+        find /usr/lib/systemd/system/ -name "scouter-server.service" -exec echo "==== PATH : {} ==== $(date)" \; -exec cat {} \; >> $sc_infotxt
         systemctl daemon-reload
         systemctl enable --now scouter-server
-        echo "==== scouter-server.service ==== $(date)" >> $infotxt
-        systemctl status scouter-server | head -10 >> $infotxt
+        echo "==== scouter-server.service ==== $(date)" >> $sc_infotxt
+        systemctl status scouter-server | head -10 >> $sc_infotxt
         echo "[INFO] Scouter-Server 설정이 완료 되었습니다. 방화벽(firewalld) 설정을 진행 합니다."
         if [ $(systemctl is-active firewalld) == "inactive" ] ; then
           echo "[WARNING] 방화벽 실행 후 가능합니다."
@@ -108,24 +108,25 @@ WantedBy=multi-user.target" > /usr/lib/systemd/system/scouter-server.service
         firewall-cmd --reload
         firewall-cmd --list-all
         echo "[INFO] 방화벽 설정이 완료 되었습니다. (6100 TCP/UDP 허용 정책 필요)"
+        sleep 1
         continue
         ;;
       2)
         echo "[INFO] Agent-Host 설정를 시작 합니다."
-        if [ ! -f "$dir1/agent.host/conf/scouter.conf" ] ; then
+        if [ ! -f "$sc_dir/agent.host/conf/scouter.conf" ] ; then
           echo "[WARNING] 설정파일이 없습니다."
         fi
-        read -p "Scouter-Server의 IP를 입력 해주세요 (ex : `ip -br addr | grep UP | grep -oP "\d+\.\d+\.\d+\.\d+"`) : " serverip1
-        sed -i "s@serverip@$serverip1@g" $dir1/agent.host/conf/scouter.conf
-        find $dir1/agent.host -type f -name "scouter.conf" -exec echo "==== PATH : {} ==== $(date)" \; -exec cat {} \; | grep -v "^#\|^$" >> $infotxt
+        read -p "Scouter-Server의 IP를 입력 해주세요 (ex : `ip -br addr | grep UP | grep -oP "\d+\.\d+\.\d+\.\d+"`) : " serverip
+          sed -i "s@serverip@$serverip@g" $sc_dir/agent.host/conf/scouter.conf && sleep 1
+        find $sc_dir/agent.host -type f -name "scouter.conf" -exec echo "==== PATH : {} ==== $(date)" \; -exec cat {} \; | grep -v "^#\|^$" >> $sc_infotxt
         echo -e "[Unit]
 Description=Scouter Host Service
 After=network.target
 
 [Service]
-WorkingDirectory=$dir1/agent.host
-User=$svcid
-Group=$svcid
+WorkingDirectory=$sc_dir/agent.host
+User=$sc_svcid
+Group=$sc_svcid
 ExecStart=/app/jdk/bin/java -classpath ./scouter.host.jar scouter.boot.Boot ./lib
 ExecStop=/usr/bin/rm -f *.scouter
 Restart=always
@@ -134,21 +135,49 @@ StandardError=journal
 
 [Install]
 WantedBy=multi-user.target" > /usr/lib/systemd/system/scouter-host.service
-        find /usr/lib/systemd/system/ -name "scouter-host.service" -exec echo "==== PATH : {} ==== $(date)" \; -exec cat {} \; >> $infotxt
+        find /usr/lib/systemd/system/ -name "scouter-host.service" -exec echo "==== PATH : {} ==== $(date)" \; -exec cat {} \; >> $sc_infotxt
         systemctl daemon-reload
         systemctl enable --now scouter-host
-        echo "==== scouter-host.service ==== $(date)" >> $infotxt
-        systemctl status scouter-host | head -10 >> $infotxt
+        echo "==== scouter-host.service ==== $(date)" >> $sc_infotxt
+        systemctl status scouter-host | head -10 >> $sc_infotxt
+        sleep 1
         continue
         ;;
       3)
         echo "[INFO] Agent-Java 설정를 시작 합니다."
-        echo "준비중"
-        #obj_name=objtmpname
-        #net_collector_ip=serverip
+        echo "[NOTICE] object 이름은 중복되지 않아야 합니다."
+        read -p "(ex : Project1-WAS-01) : " objname
+          sed -i "s@objtmpname@$objname@g" $sc_dir/agent.java/conf/scouter.conf && sleep 1
+        echo "[INFO] Server IP로 사용했던 IP는 $serverip 입니다.."
+        read -p "Scouter-Server의 IP를 입력 해주세요 : " serverip
+          sed -i "s@serverip@$serverip@g" $sc_dir/agent.java/conf/scouter.conf && sleep 1
+        find $sc_dir/agent.java -type f -name "scouter.conf" -exec echo "==== PATH : {} ==== $(date)" \; -exec cat {} \; | grep -v "^#\|^$" >> $sc_infotxt
+        echo -e "\n[NOTICE]\n1. tomcat 재실행이 필요 합니다.\n2. setenv.sh, startup.sh, catalina.sh 중 선택해서 하나만 등록 하면 됩니다.\n3. 동작을 위해 scouter-host 를 재실행 해야 합니다."
+        sleep 1
+        find /* -type f -name "catalina.sh" | while read file ; do echo "${file/catalina.sh/setenv.sh}" ;done
+        read -p "setenv.sh 파일 맨 마지막 줄에 Scouter 설정을 추가 하시겠습니까? (y/n) : " var
+        case $var in
+          y | Y )
+            read -p "[INFO] setenv.sh 절대 경로 입력 : " path
+            echo "[INFO] $path 파일에 설정을 추가 합니다."
+            cp -ar $path $path.$(date +'%Y-%m-%d')
+            echo '#===========================================================
+#Scouter Setting
+#===========================================================
+export SCOUTER_AGENT_DIR=/app/scouter/agent.java
+export JAVA_OPTS="$JAVA_OPTS -javaagent:${SCOUTER_AGENT_DIR}/scouter.agent.jar"
+export JAVA_OPTS="$JAVA_OPTS -Dscouter.config=${SCOUTER_AGENT_DIR}/conf/scouter.conf"' >> $path
+            ;;
+          n | N | * )
+            echo "need a continue?"
+            ;;
+        esac
+        echo "[INFO] scouter-host 서비스를 재시작 합니다."
+        systemctl restart scouter-host
         continue
         ;;
       0)
+        echo "[INFO] scouter client 기본 계정과 패스워드는 admin, admin 입니다."
         echo "[INFO] 스크립트 종료"
         exit 0
         ;;
